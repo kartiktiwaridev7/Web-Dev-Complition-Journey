63ac0a8b9ac520d9181e2419fa342940bc3d85c6
Kartik Tiwari <kartiktiwaridev7@gmail.com>
2026-03-22 21:20:40 +0530

    Add files via upload

--- Day - 1/script.js
+++ Day - 1/script.js
@@ -1,3 +1,30 @@
-console.log(a)
+// console.log(a)
 
-var a = 12;
+// var a = 12;
+// var x = 1; // Global
+
+// {
+//     var x = 2 // Global var didn't respect bolck so it became global .
+// }
+// console .log(x)
+
+// let a = 20 ; // Global
+// {
+//     let a = 10;  // Block
+//     console.log(a)
+
+// }
+// console.log(a)
+
+// if (true){
+//     let a = 20
+//     var b = 10
+// }
+// // console .log(a)
+// console.log(b)
+// const person = {name:"harsh"}
+// person.name = "Sharma"
+// // person = {}
+let a = 12;
+let b = a;
+a = 2+a
